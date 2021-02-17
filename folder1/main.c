eb9cf7c4823de5ce94472c8df8e3bfcf54de27d7
151090 <m.helmy.amit@gmail.com>
2021-02-17 20:04:48 +0200

    test folders

---
+++ folder1/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char x;
+    int y,z,r;
+    printf("Enter the Operator \n");
+    scanf("%c",&x);
+
+     //printf("Enter Number_1 \n");
+    scanf("%d",&y);
+
+     printf("Enter Number_2 \n");
+    scanf("%d",&z);
+
+    switch (x)
+    {
+
+
+
+case'+':
+    r=y+z;
+    break;
+
+case'-':
+    r=y-z;
+
+    break;
+
+case'*':
+    r=y*z;
+    break;
+
+case'/':
+    r=y/z;
+    break;
+
+    }
+
+     printf("The result = %d \n",r);
+
+
+    return 0;
+}
